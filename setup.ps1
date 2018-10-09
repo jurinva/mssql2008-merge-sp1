d7361c2ffd1f948264e34a1d628290d4775c625b
jurinva <jurinva@gmail.com>
2018-10-09 18:49:25 +0300

    Add functions

--- setup.ps1
+++ setup.ps1
@@ -1,25 +1,38 @@
-$os_type = (Get-WmiObject -Class Win32_ComputerSystem).SystemType -match ‘(x64)’
-
-if ($os_type -eq "True") {
-  Invoke-WebRequest -Uri https://download.microsoft.com/download/7/7/6/776727E8-57EE-4AB5-BC69-6CCDF04A2A70/SQLServer2008R2SP1-KB2528583-x64-ENU.exe -OutFile $env:USERPROFILE\Downloads\SQLServer2008R2SP1-KB2528583-x64-ENU.exe
-  SQLServer2008R2SP1-KB2528583-x64-ENU.exe /x:C:\SQLServer2008R2_SP1\SP
-  robocopy C:\SQLServer2008R2_SP1\SP C:\SQLServer2008R2_SP1 Setup.exe
-  robocopy C:\SQLServer2008R2_SP1\SP\x64 C:\SQLServer2008R2_SP1\x64 /XF Microsoft.SQL.Chainer.PackageData.dll
-else {
-  $os_type = (Get-WmiObject -Class Win32_ComputerSystem).SystemType -match ‘(x86)’
-    if ($os_type -eq "True") {
-      Invoke-WebRequest -Uri https://download.microsoft.com/download/7/7/6/776727E8-57EE-4AB5-BC69-6CCDF04A2A70/SQLServer2008R2SP1-KB2528583-x86-ENU.exe -OutFile $env:USERPROFILE\Downloads\SQLServer2008R2SP1-KB2528583-x86-ENU.exe
-      SQLServer2008R2SP1-KB2528583-x86-ENU.exe /x:C:\SQLServer2008R2_SP1\SP
-      robocopy C:\SQLServer2008R2_SP1\SP C:\SQLServer2008R2_SP1 Setup.exe
-      robocopy C:\SQLServer2008R2_SP1\SP\x86 C:\SQLServer2008R2_SP1\x86 /XF Microsoft.SQL.Chainer.PackageData.dll
-    }
+function Get-Arch {
+  $os_type = (Get-WmiObject -Class Win32_ComputerSystem).SystemType -match ‘(x64)’
+  if ($os_type -eq "True") {
+    return x64
+  else {
+    $os_type = (Get-WmiObject -Class Win32_ComputerSystem).SystemType -match ‘(x86)’
+      if ($os_type -eq "True") {
+        return x64
+      }
+  }
 }
 
-if(![System.IO.File]::Exists($path)){
-    # file with path $path doesn't exist
+$arch = Get-Arch
+$file = SQLServer2008R2SP1-KB2528583-$arch-ENU.exe
+$uri = https://download.microsoft.com/download/7/7/6/776727E8-57EE-4AB5-BC69-6CCDF04A2A70/$file
+#$x64uri = https://download.microsoft.com/download/7/7/6/776727E8-57EE-4AB5-BC69-6CCDF04A2A70/SQLServer2008R2SP1-KB2528583-x64-ENU.exe
+
+function Merge ($a, $f, $u) {
+  Invoke-WebRequest -Uri $u -OutFile $env:USERPROFILE\Downloads\$f
+  Start-Process "$f /x:C:\SQLServer2008R2_SP1\SP"
+  Start-Process "robocopy C:\SQLServer2008R2_SP1\SP C:\SQLServer2008R2_SP1 Setup.exe"
+  Start-Process "robocopy C:\SQLServer2008R2_SP1\SP\$a C:\SQLServer2008R2_SP1\$a /XF Microsoft.SQL.Chainer.PackageData.dll"
 }
 
+function Check-IniFile ($a) {
+  if(![System.IO.File]::Exists("C:\SQLServer2008R2_SP1\$a")){
+    Add-Content C:\SQLServer2008R2_SP1\$a 'PCUSOURCE=".\SP"'
+  }
+}
 
-setup.exe
+function Main ($arch, $file, $uri) {
+  Merge $arch $file $uri
+  Check-IniFile $arch
+  Start-Process "C:\SQLServer2008R2_SP1\setup.exe"
+}
 
+Main $arch $file $uri
 
